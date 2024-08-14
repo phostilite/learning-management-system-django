f8ca0892c722b96acc755b4a0c2a4f9cd51b4e8c
Priyanshu Sharma <priyanshu.sharma9@s.amity.edu>
2024-08-14 11:29:38 +0530

    five

--- courses/models.py
+++ courses/models.py
@@ -20,6 +20,8 @@ class Course(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='created_courses')
     is_published = models.BooleanField(default=False)
+    scorm_url = models.URLField(null=True, blank=True)
+
 
     def __str__(self):
         return self.title
