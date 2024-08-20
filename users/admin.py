b51b586fabd6f0ace9ccf5ec583e5570c4c470ba
Priyanshu Sharma <priyanshu.sharma9@s.amity.edu>
2024-08-21 03:49:12 +0530

    Register SCORMUserProfile model in admin.py
    - Added SCORMUserProfile model to the Django admin site for easier management.

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin  
 
-from .models import User, Learner, Administrator, Supervisor, Facilitator
+from .models import User, Learner, Administrator, Supervisor, Facilitator, SCORMUserProfile
 
 admin.site.register(Learner)
 admin.site.register(Administrator)
 admin.site.register(Supervisor)
 admin.site.register(Facilitator)
+admin.site.register(SCORMUserProfile)
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
